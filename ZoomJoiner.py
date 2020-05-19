33bc2ebfa1fd4d890ae1ebd3978ac5a2e62856e5
MrCasco <ernesto.casco.velazquez@hotmail>
2020-05-19 12:34:18 -0500

    Ya se puede usar teclado y mouse con python

--- ZoomJoiner.py
+++ ZoomJoiner.py
@@ -1,48 +1,64 @@
-from pynput.mouse import Button, Controller
-from pynput.keyboard import Key
+from pynput.keyboard import Key, Controller
+import pyautogui
 import time
 import webbrowser
 
-mouse = Controller()
-def joinZoom(link):
-    webbrowser.open(link)
-    mouse.position = (695, 528)
-    for x in range(3, 5):
-        time.sleep(x)
-        mouse.click(Button.left, 1)
-
+keyboard = Controller()
 links = []
 hours = []
-joined = False
 classesTaken = 0
 totalClasses = 0
-now = time.localtime()
-minimumHours = 24
-minimumMins = 59
 index = 0
-dia = input("What day is today? ")
-f = open("../../Schedule/"+dia+".txt", "r")
-for x in f:
-    links.append(x.split(',')[0])
-    hours.append(x.split(',')[1])
-    hour = int(x.split(',')[1].split(':')[0])
-    minute = int(x.split(',')[1].split(':')[1])
-    hoursToNextClass = hour - now.tm_hour
-    minsToNextClass = minute - now.tm_min
-    if hoursToNextClass >= 0 and hoursToNextClass <= minimumHours:
-        if minsToNextClass >= 0 and minsToNextClass <= minimumMins:
+
+def joinZoom(link):
+    webbrowser.open(link)
+    time.sleep(10)
+    for x in range(3):
+        keyboard.press(Key.tab)
+        keyboard.release(Key.tab)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    pyautogui.click(682, 382)
+
+def getNextClass(dia):
+    minimumHours = 24
+    minimumMins = 59
+    totalClasses = 0
+    now = time.localtime()
+    f = open("../../Schedule/"+dia+".txt", "r")
+    for x in f:
+        links.append(x.split(',')[0])
+        hours.append(x.split(',')[1])
+        hour = int(x.split(',')[1].split(':')[0])
+        minute = int(x.split(',')[1].split(':')[1])
+        hoursToNextClass = hour - now.tm_hour
+        minsToNextClass = minute - now.tm_min
+        if hoursToNextClass == 0 and minsToNextClass >= 0 and minsToNextClass < minimumMins:
+            minimumHours = 0
+            minimumMins = minsToNextClass
+            index = totalClasses
+        elif hoursToNextClass > 0 and hoursToNextClass <= minimumHours and minsToNextClass >= 0 and minsToNextClass < minimumMins:
             minimumHours = hoursToNextClass
             minimumMins = minsToNextClass
             index = totalClasses
-    totalClasses += 1
+        totalClasses += 1
+    return index, totalClasses
 
+day = input("What day is today? ")
+index, totalClasses = getNextClass(day)
 nextHour = hours[index].split(':')
-print("Ya funciona! Espera tu siguiente clase (no cierres esta ventana)")
-while joined == False:
+print("It's working! Your next class is at"+hours[index]+"(do not close this window)")
+print("Total classes today: ", totalClasses)
+while classesTaken <= totalClasses:
     time.sleep(20)
     now = time.localtime()
-    if now.tm_hour == int(nextHour[0]) and now.tm_min == int(nextHour[1]):
+    inSession = False
+    if now.tm_hour == int(nextHour[0]) and now.tm_min == int(nextHour[1]) and inSession == False:
+        joinZoom(links[index])
         classesTaken += 1;
-        joinZoom(links[classesTaken])
-        print("now")
-        joined = True
+        print("Joined!")
+        getNextClass(day)
+        print("Your next class is at"+hours[index]+"(do not close this window)")
+        print("You're missing "+(totalClasses-classesTaken)+" classes")
+        inSession = True
+        time.sleep(60)
